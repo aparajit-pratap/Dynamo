4a2b41c98a60c4505747c14fc4b93016ec25fbd9
aparajit <aparajit.pratap@autodesk.com>
2014-01-16 18:26:33 +0800

    Added new DynamoWrapper project - a wrapper class of Dynamo sandbox to integrate it with external projects

---
+++ src/DynamoWrapper/DynamoWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
+
+
+using Dynamo;
+using Dynamo.Services;
+using Dynamo.Controls;
+using Dynamo.Models;
+using Dynamo.Selection;
+using Dynamo.Utilities;
+using Dynamo.ViewModels;
+
+namespace DynamoWrapper
+{
+    public class DynamoCSharpWrapper
+    {
+        protected static DynamoController Controller { get; set; }
+
+        protected static DynamoViewModel Vm { get; set; }
+
+        protected static DynamoView Ui { get; set; }
+
+        protected static DynamoModel Model { get; set; }
+
+        public static void StartUp()
+        {
+            
+            Controller = DynamoController.MakeSandbox();
+            //controller.Testing = true;
+
+            //create the view   
+            Ui = new DynamoView();
+            Ui.DataContext = Controller.DynamoViewModel;
+            Vm = Controller.DynamoViewModel;
+            Model = Controller.DynamoModel;
+            Controller.UIDispatcher = Ui.Dispatcher;
+            Ui.Show();
+
+            
+            //SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+        }
+    }
+}
