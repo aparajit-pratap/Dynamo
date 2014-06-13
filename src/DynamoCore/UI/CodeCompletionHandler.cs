5d38e4d352b7d6c9fa7499a2e5c030d03b30a815
aparajit <aparajit.pratap@autodesk.com>
2014-06-13 16:38:19 +0800

    Added custom popup window for completion list

---
+++ src/DynamoCore/UI/CodeCompletionHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Dynamo.UI
+{
+    class CodeCompletionHandler
+    {
+        TextBox textBox;
+
+        public CodeCompletionHandler(TextBox tb)
+        {
+            Popup p = new Popup();
+            var list = new ListBox();
+            list.KeyDown += list_KeyDown;
+            list.Items.Add("Hey");
+            p.Child = list;
+
+            p.Placement = PlacementMode.Custom;
+            p.PlacementTarget = tb;
+            textBox = tb;
+            
+            
+            p.CustomPopupPlacementCallback = delegate(Size popupSize, Size targetSize, Point offset)
+            {
+                return new[] { new CustomPopupPlacement() };
+            };
+            
+            p.IsOpen = true;
+
+        }
+
+        void list_KeyDown(object sender, KeyEventArgs e)
+        {
+            var item = (sender as ListBox).SelectedItem as string;
+            if (item != null)
+            {
+                this.textBox.Text.Insert(textBox.CaretIndex, item);
+            }
+            ((sender as ListBox).Parent as Popup).IsOpen = false;
+        }
+
+    }
+}
