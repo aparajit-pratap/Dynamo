96577d0b73103b6473b9c57a425ce15555327d28
aparajit <aparajit.pratap@autodesk.com>
2016-02-04 10:30:51 +0800

    fix for List.ShiftIndices

--- src/Libraries/CoreNodes/List.cs
+++ src/Libraries/CoreNodes/List.cs
@@ -393,6 +393,11 @@ namespace DSCore
         /// <search>shift,offset</search>
         public static IList ShiftIndices(IList list, int amount)
         {
+            if (System.Math.Abs(amount) > list.Count)
+            {
+                amount = amount%list.Count;
+            }
+
             if (amount == 0)
                 return list;
 
