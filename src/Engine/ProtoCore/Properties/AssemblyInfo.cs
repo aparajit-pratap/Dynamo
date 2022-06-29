c5200a8afc0ff6ee7dc79a2d61953c3689cb4648
Aparajit Pratap <aparajit.pratap@autodesk.com>
2022-06-29 16:45:59 -0400

    align node previews with number format selected in preferences

--- src/Engine/ProtoCore/Properties/AssemblyInfo.cs
+++ src/Engine/ProtoCore/Properties/AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("CodeGenILTests")]
 [assembly: InternalsVisibleTo("ProtoTestFx")]
 [assembly:InternalsVisibleTo("DynamoCore")]
+[assembly:InternalsVisibleTo("DynamoCoreWpf")]
 [assembly: InternalsVisibleTo("DynamoCoreTests")]
 [assembly: InternalsVisibleTo("DynamoCoreWpfTests")]
 [assembly: InternalsVisibleTo("ProtoImperative")]
